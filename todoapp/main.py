96014ae587fffc8e00917e2467e52049846321ae
parvinghasemi <mghasemi.parvin@gmail.com>
2024-11-04 01:37:06 +0100

    add put request: update a todo

--- todoapp/main.py
+++ todoapp/main.py
@@ -47,8 +47,25 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     raise HTTPException(status_code=404, detail="To-do not found.")
 
 
-@app.post("/todo", status_code=status.HTTP_201_CREATED)
+@app.post("/todos", status_code=status.HTTP_201_CREATED)
 async def create_new_todo(db: db_dependency, todo_request: TodosRequest):
     todo_model = Todos(**todo_request.dict())
     db.add(todo_model)
     db.commit()
+
+
+@app.put("/todos/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db: db_dependency, todo_request: TodosRequest, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail="To-do not found.")
+
+    todo_model.title = todo_request.title
+    todo_model.description = todo_request.description
+    todo_model.priority = todo_request.priority
+    todo_model.complete = todo_request.complete
+
+    db.add(todo_model)
+    db.commit()
+    
+
