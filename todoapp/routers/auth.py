0a91f554872b2c71361a7d75d916fd8f423e7165
parvinghasemi <mghasemi.parvin@gmail.com>
2024-11-05 14:19:28 +0100

    enhance the jwt token response

--- todoapp/routers/auth.py
+++ todoapp/routers/auth.py
@@ -28,6 +28,11 @@ class CreateUserRequest(BaseModel):
     role: str
 
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
 def get_db():
     db = SessionLocal()
     try:
@@ -70,12 +75,12 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
     db.commit()
     
 
-@router.post("/token")
+@router.post("/token", response_model=Token)
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         return "Failed Authentication"
 
     token = create_access_token(user.username, user.id, timedelta(minutes=20))
-    return token
+    return {'access_token': token, 'token_type': 'bearer'}
 
