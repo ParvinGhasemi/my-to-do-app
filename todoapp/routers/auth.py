7bf5dea813b966a36ca90e55f925b4d8bdd39a0c
parvinghasemi <mghasemi.parvin@gmail.com>
2024-11-05 14:01:24 +0100

    jwt: encode a json web token

--- todoapp/routers/auth.py
+++ todoapp/routers/auth.py
@@ -7,9 +7,16 @@ from models import Users
 from passlib.context import CryptContext
 from starlette import status
 from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
+from datetime import timedelta, datetime, timezone
+
 
 
 router = APIRouter()
+
+SECRET_KEY = "ba5bea8a61872e668a3f1e442f8aac2a76ebff4ba2540bfcaa4a399824d2e1c9"
+ALGORITHM = "HS256"
+
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 class CreateUserRequest(BaseModel):
@@ -37,7 +44,15 @@ def authenticate_user(username: str, password: str, db):
         return False
     if not bcrypt_context.verify(password, user.hashed_password):
         False
-    return True
+    return user
+
+
+def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+    encode = {'sub': username, 'id': user_id}
+    expires = datetime.now(timezone.utc) + expires_delta    # old version: datetime.utcnow()
+    encode.update({'exp': expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
 
 
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
@@ -60,5 +75,7 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         return "Failed Authentication"
-    return "Successful Authentication"
+
+    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+    return token
 
