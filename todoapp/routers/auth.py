5690194040a340f95edb0ec870513987f177735d
parvinghasemi <mghasemi.parvin@gmail.com>
2024-11-05 15:04:10 +0100

    decode a jwt-json web token

--- todoapp/routers/auth.py
+++ todoapp/routers/auth.py
@@ -1,15 +1,14 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from database import SessionLocal
 from models import Users
 from passlib.context import CryptContext
 from starlette import status
-from fastapi.security import OAuth2PasswordRequestForm
-from jose import jwt
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt, JWTError
 from datetime import timedelta, datetime, timezone
-
 
 
 router = APIRouter()
@@ -18,6 +17,8 @@ SECRET_KEY = "ba5bea8a61872e668a3f1e442f8aac2a76ebff4ba2540bfcaa4a399824d2e1c9"
 ALGORITHM = "HS256"
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')  # this parameter contains the url that the client will send to our fastapi application
+
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -57,6 +58,20 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta):
     expires = datetime.now(timezone.utc) + expires_delta    # old version: datetime.utcnow()
     encode.update({'exp': expires})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithm=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user.')
+        return {'username': username, 'id': user_id}
+    
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user.')
+
 
 
 
