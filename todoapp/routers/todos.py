82f8234be01efba133f633b134d858b27106924d
parvinghasemi <mghasemi.parvin@gmail.com>
2024-11-06 09:19:18 +0100

    create/post a todo with user id

--- todoapp/routers/todos.py
+++ todoapp/routers/todos.py
@@ -7,6 +7,7 @@ from fastapi import APIRouter, Depends, HTTPException, Path
 from starlette import status
 from models import Todos
 from database import engine, SessionLocal
+from .auth import get_current_user
 
 
 router = APIRouter()
@@ -27,6 +28,7 @@ def get_db():
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)] 
 
 @router.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
@@ -42,8 +44,10 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
 
 
 @router.post("/todos", status_code=status.HTTP_201_CREATED)
-async def create_new_todo(db: db_dependency, todo_request: TodosRequest):
-    todo_model = Todos(**todo_request.dict())
+async def create_todo(user: user_dependency, db: db_dependency, todo_request: TodosRequest):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    todo_model = Todos(**todo_request.dict(), owner_id = user.get('id'))
     db.add(todo_model)
     db.commit()
 
