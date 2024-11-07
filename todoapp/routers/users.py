7a1addcdef9635bf22310ffb7ef8b976a138f209
parvinghasemi <mghasemi.parvin@gmail.com>
2024-11-07 13:54:01 +0100

    added changing user password

--- todoapp/routers/users.py
+++ todoapp/routers/users.py
@@ -1,14 +1,13 @@
 from typing import Annotated
-
 from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
-
 from fastapi import APIRouter, Depends, HTTPException, Path
 from starlette import status
 from models import Todos, Users
 from database import engine, SessionLocal
 from .auth import get_current_user
 from passlib.context import CryptContext
+
 
 router = APIRouter(
     prefix='/user',
@@ -27,11 +26,28 @@ user_dependency = Annotated[dict, Depends(get_current_user)]
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 
+class UserVerification(BaseModel):
+    password: str
+    new_password: str = Field(min_length=6)
+
+
 @router.get('/', status_code=status.HTTP_200_OK)
-async def read_user_info(user:user_dependency, db: db_dependency):
+async def get_user_info(user:user_dependency, db: db_dependency):
     if user is None:
         raise HTTPException(status_code=401, detail='Authentication Failed.')
-    return db.query(Users).filter(Users.id == user.get(id)).first()
+    return db.query(Users).filter(Users.id == user.get('id')).first()
 
 
+@router.put('/password', status_code=status.HTTP_204_NO_CONTENT)
+async def update_user_password(user:user_dependency, db: db_dependency, user_verification: UserVerification): 
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed.')
+    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+
+    if not bcrypt_context.verify(user_verification.password, user_model.hashed_password):
+        raise HTTPException(status_code=401, detail='Current password is not correct.')
+    user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
+
+    db.add(user_model)
+    db.commit()
 
