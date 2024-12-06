0df548e796fe74d8eb004be47a65342f40ed19c4
parvinghasemi <mghasemi.parvin@gmail.com>
2024-12-06 13:48:03 +0100

    added tests for auth: async get_current_user function

--- todoapp/test/test_auth.py
+++ todoapp/test/test_auth.py
@@ -1,8 +1,8 @@
 from .utils import *
-from ..routers.auth import get_db, authenticate_user, create_access_token, SECRET_KEY, ALGORITHM
+from ..routers.auth import get_db, authenticate_user, create_access_token, SECRET_KEY, ALGORITHM, get_current_user
 from datetime import timedelta
 from jose import jwt
-
+import pytest   # to test async functions with pytest-asyncio: so the test is not skipped
 
 app.dependency_overrides[get_db] = override_get_db
 
@@ -33,3 +33,11 @@ def test_create_access_token():
     assert decoded_token['id'] == user_id
     assert decoded_token['role'] == role
     
+
+@pytest.mark.asyncio    
+async def test_get_current_user():
+    to_encode = {'sub': 'testuser', 'id': 1, 'role': 'admin'}
+    token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    
+    user = await get_current_user(token=token)
+    assert user == {'username': 'testuser', 'id': 1, 'user_role': 'admin'}
